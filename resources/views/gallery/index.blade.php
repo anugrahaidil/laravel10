50d5bdedbdbb673100d58ef9a683369363d66200
anugrahaidil <anugrahaidilfitri@mail.ugm.ac.id>
2024-12-02 12:54:46 +0700

    baru2

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -10,22 +10,22 @@
             </div>
             <div class="card-body">
                 <div class="row">
-                    @if(count($galleries) > 0)
+                    @if(!empty($galleries) && count($galleries) > 0)
                         @foreach($galleries as $gallery)
                             <div class="col-sm-4 mb-4">
                                 <div class="card">
-                                    <a href="{{ asset('storage/posts_image/' . $gallery->picture) }}" data-lightbox="roadtrip" data-title="{{ $gallery->description }}">
-                                        <img src="{{ asset('storage/posts_image/' . $gallery->picture) }}" alt="Gallery Image" class="card-img-top img-fluid">
+                                    <a href="{{ asset('storage/posts_image/' . $gallery['picture']) }}" data-lightbox="roadtrip" data-title="{{ $gallery['description'] }}">
+                                        <img src="{{ asset('storage/posts_image/' . $gallery['picture']) }}" alt="Gallery Image" class="card-img-top img-fluid">
                                     </a>
                                     <div class="card-body">
-                                        <h5 class="card-title">{{ $gallery->title }}</h5>
-                                        <p class="card-text">{{ $gallery->description }}</p>
+                                        <h5 class="card-title">{{ $gallery['title'] }}</h5>
+                                        <p class="card-text">{{ $gallery['description'] }}</p>
                                         
                                         <!-- Tombol Edit -->
-                                        <a href="{{ route('gallery.edit', $gallery->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                                        <a href="{{ route('gallery.edit', $gallery['id']) }}" class="btn btn-warning btn-sm">Edit</a>
                                         
                                         <!-- Tombol Hapus -->
-                                        <form action="{{ route('gallery.destroy', $gallery->id) }}" method="POST" style="display:inline;">
+                                        <form action="{{ route('gallery.destroy', $gallery['id']) }}" method="POST" style="display:inline;">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus gambar ini?')">Hapus</button>
@@ -38,9 +38,13 @@
                         <h3>Tidak ada data</h3>
                     @endif
                 </div>
-                <div class="d-flex justify-content-center mt-3">
-                    {{ $galleries->links() }}
-                </div>
+                <!-- Pagination -->
+                @if(!empty($galleries))
+                    <div class="d-flex justify-content-center mt-3">
+                        {{-- Tambahkan link ke API pagination di sini jika diperlukan --}}
+                        {{-- Implementasi pagination manual jika tidak didukung --}}
+                    </div>
+                @endif
             </div>
         </div>
     </div>
